b3e3027225a2fd3fdc18278561b91cd98c5311c9
Andreas Maunz <andreas@maunz.de>
2010-07-12 16:34:25 +0200

    Updated doc

--- liblast/Mainpage.h
+++ liblast/Mainpage.h
@@ -94,43 +94,51 @@
  * using namespace std;
  *
  * Last* MyFminer;
- * int main(int argc, char *argv[], char *envp) {
- *   MyFminer= new Last();
- *   MyFminer->AddCompound ("COC1=CC=C(C=C1)C2=NC(=C([NH]2)C3=CC=CC=C3)C4=CC=CC=C4" , 1);
- *   MyFminer->AddCompound ("O=C1NC(=S)NC(=O)C1C(=O)NC2=CC=CC=C2" , 2);
- *      // ... continue adding compounds
- *   MyFminer->AddActivity(1.0, 1);
- *   MyFminer->AddActivity(0.0, 2);
- *      // ... continue adding activities (1.0 for active, 0.0 for inactive)
- *   cerr << MyFminer->GetNoCompounds() << " compounds" << endl;
- *   for ( int j = 0; j < (int) MyFminer->GetNoRootNodes(); j++ ) {
- *      MyFminer->MineRoot(j);
- *   }
- *   delete MyFminer;
+ * int main(int argc, char *argv[], char *envp[]) {
+     * MyFminer= new Last();
+     * MyFminer = Last::Last.new()
+     * MyFminer->AddCompound ("O=C(C(C(C=C3)=CC=C3O)=CO2)C1=C2C=C(O)C=C1O" , 1);
+     * MyFminer->AddCompound ("Oc1ccc(cc1)[C@@H]2Cc3ccc(O)cc3OC2" , 2);
+     * MyFminer->AddCompound ("O=C1C(C3=CC=C(O)C=C3)=COC2=C1C=CC(O)=C2" , 3);
+     * MyFminer->AddCompound ("O=C1C(C3=CC=C(OC)C=C3)=COC2=C1C=CC(O)=C2" , 4);
+     * MyFminer->AddCompound ("OC1=CC=C(CCCCCCCC)C=C1" , 5);
+     * MyFminer->AddCompound ("C1(C=CC=CC=1C(=C(Cl)Cl)C2=CC=C(C=C2)Cl)Cl" , 6);
+     * MyFminer->AddCompound ("O=C(C1=C(C=CC=C1)C(=O)OCC(CCCC)CC)OCC(CCCC)CC" , 7);
+     * MyFminer->AddCompound ("Oc1cc(O)cc2CCCCC[C@@H](O)CCC[C@H](C)OC(=O)c12" , 8);
+     * MyFminer->AddCompound ("O=C1C2=C(C=C(C=C2O)O)OC(=C1O)C3=CC(=C(C=C3)O)O" , 9);
+     * MyFminer->AddCompound ("C1(=C(C(=O)C2=C(O1)C=C(C=C2)O)O)C3=CC(O)=C(C=C3)O" , 10);
+     * //... continue adding compounds
+     * MyFminer->AddActivity((bool) 1.0, 1);
+     * MyFminer->AddActivity((bool) 1.0, 2);
+     * MyFminer->AddActivity((bool) 1.0, 3);
+     * MyFminer->AddActivity((bool) 1.0, 4);
+     * MyFminer->AddActivity((bool) 1.0, 5);
+     * MyFminer->AddActivity((bool) 1.0, 6);
+     * MyFminer->AddActivity((bool) 1.0, 7);
+     * MyFminer->AddActivity((bool) 1.0, 8);
+     * MyFminer->AddActivity((bool) 0.0, 9);
+     * MyFminer->AddActivity((bool) 0.0, 10);
+     * //... continue adding activities (1.0 for active, 0.0 for inactive)
+     * cerr << MyFminer->GetNoCompounds() << " compounds" << endl;
+     * MyFminer->SetMaxHops(25);
+     * // gather results for every root node in vector instead of immediate output
+     * MyFminer->SetConsoleOut(false);
+     * for ( int j = 0; j < (int) MyFminer->GetNoRootNodes(); j++ ) {
+     *    vector<string>* result = MyFminer->MineRoot(j);
+     *    for ( int i = 0; i < result->size(); i++) {
+     *       cout << (*result)[i] << endl;
+     *    }
+     * }
+     * delete MyFminer;
  * }
- *
  *  \endcode
  *
  * \subsubsection Ruby Ruby
  *
  * This example assumes that you have created ruby bindings using <code>make ruby</code>.
  * \code
- *
- * require 'last'
- * MyFminer = Last::Last.new()
- * MyFminer.AddCompound("COC1=CC=C(C=C1)C2=NC(=C([NH]2)C3=CC=CC=C3)C4=CC=CC=C4" , 1)
- * MyFminer.AddCompound("O=C1NC(=S)NC(=O)C1C(=O)NC2=CC=CC=C2" , 2)
- *    # ... continue adding compounds
- * MyFminer.AddActivity(1.0, 1)
- * MyFminer.AddActivity(0.0, 2)
- *    # ... continue adding activities (1.0 for active, 0.0 for inactive)
- * print MyFminer.GetNoCompounds()  
- * puts " compounds"
- * (0 .. MyFminer.GetNoRootNodes()-1).each do |j|
- *    MyFminer.MineRoot(j)
- * end
- *
- *  \endcode
+ * bla
+ * \endcode
  *
  * \subsubsection Const Description of Constructors and Options
  * 
