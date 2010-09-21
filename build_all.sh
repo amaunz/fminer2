e572c3c2cd3ed726476e11d15922fe84a7f6cfba
Andreas Maunz <andreas@maunz.de>
2010-09-21 20:25:09 +0200

    build script improved

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
+# Try running this file for a complete build
 
-./liblast/configure
-./libbbrc/configure
-./fminer/configure
+make -C liblast clean >/dev/null 2>&1
+make -C libbbrc clean >/dev/null 2>&1
+make -C fminer clean >/dev/null 2>&1
 
-make -C liblast/
-make -C libbbrc/
-make -C fminer/
+cd ./liblast/ ; ./configure >/dev/null
+if [ $? -ne 0 ]; then
+  echo "Configuration failed."
+  exit 1
+fi
+cd -
+make -C liblast/ > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Build failed."
+  exit 1
+fi
+
+
+cd ./libbbrc/ ; ./configure >/dev/null
+if [ $? -ne 0 ]; then
+  echo "Configuration failed."
+  exit 1
+fi
+cd -
+make -C libbbrc/ > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Build failed."
+  exit 1
+fi
+
+
+cd ./fminer/ ; ./configure >/dev/null
+if [ $? -ne 0 ]; then
+  echo "Configuration failed."
+  exit 1
+fi
+cd -
+make -C fminer/ > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Build failed."
+  exit 1
+fi
