2966f77d88035c1c5c6de1d16c8e45c527e50bd7
Andreas Maunz <andreas@maunz.de>
2011-03-25 14:24:22 +0100

    Updated doc

--- libbbrc/Mainpage.h
+++ libbbrc/Mainpage.h
@@ -143,9 +143,10 @@
  *  \endcode
  *
  * Documentation for YAML can be found at: http://yaml.org/spec/cvs/current.html# (e.g. export FMINER_SMARTS=1).<br />
- * <b>FMINER_LAZAR</b>: Produce output in linfrag format which can be used as input to <code><a href="http://lazar.in-silico.de" target="_blank">Lazar</a></code> (e.g. export FMINER_LAZAR=1).<br />
- * <b>FMINER_PVALUES</b>: Produce p-values instead of chi-square values (e.g. export FMINER_PVALUES=1).<br />
- * <b>FMINER_AROMATIC_WC</b>: Allow aromatic bonds on non-aromatic bonds, i.e. aromatic wildcarding (e.g. export FMINER_AROMATIC_WC=1).
+ * <b>FMINER_SMARTS</b>      : Produce output in SMARTS format (e.g. export FMINER_SMARTS=1).<br />
+ * <b>FMINER_LAZAR</b>       : Produce output in linfrag format which can be used as input to Lazar (e.g. export FMINER_LAZAR=1).<br />
+ * <b>FMINER_PVALUES</b>     : Produce p-values instead of chi-square values (e.g. export FMINER_PVALUES=1).<br />
+ * <b>FMINER_NO_AROMATIC_WC</b>: Disallow aromatic wildcard bonds on aliphatic bonds, when aromatic perception was switched off ('-a') (e.g. export FMINER_NO_AROMATIC_WC=1).
  *
  * Note: The value you set the environment variables to is irrelevant. Use <code>unset</code> to disable the environment variables, e.g. <code>unset FMINER_LAZAR</code>.
  *
