61d3bc09543d3fb16884146b0a535bee29968307
Andreas Maunz <andreas@maunz.de>
2011-06-20 13:42:20 +0200

    Completed Test

--- fminer/test.sh
+++ fminer/test.sh
@@ -8,7 +8,7 @@ testBbrcClassification()
   $fminer $libbrc $hamster>$testdir/tmp1 2>$testdir/tmp1e
   h=`md5sum $testdir/tmp1 | sed 's/\s.*//g'`
   he=`md5sum $testdir/tmp1e | sed 's/\s.*//g'`
-  if [ "`uname -m`" = "x64" ]; then
+  if [ "`uname -m`" = "x86_64" ]; then
     assertEquals "testBbrcClassification" "$h" "3b8c0f319d38be021add34b62fda2b44"
   else
     assertEquals "testBbrcClassification" "$h" "599fb20d22d88226377047c15bfb9ca8"
@@ -21,7 +21,7 @@ testBbrcFsmClassification()
   $fminer $libbrc $fsmargs $hamster>$testdir/tmp2 2>$testdir/tmp2e
   h=`md5sum $testdir/tmp2 | sed 's/\s.*//g'`
   he=`md5sum $testdir/tmp2e | sed 's/\s.*//g'`
-  if [ "`uname -m`" = "x64" ]; then
+  if [ "`uname -m`" = "x86_64" ]; then
     assertEquals "testBbrcClassification" "$h" "576522f98236969193b8f661309ccefc"
   else
     assertEquals "testBbrcClassification" "$h" "1ce56e91303503e6fd9cb5ecf1dc14fb"
@@ -34,7 +34,7 @@ testBbrcFsmP0Classification()
   $fminer $libbrc $fsmargs $p0args $hamster>$testdir/tmp3 2>$testdir/tmp3e
   h=`md5sum $testdir/tmp3 | sed 's/\s.*//g'`
   he=`md5sum $testdir/tmp3e | sed 's/\s.*//g'`
-  if [ "`uname -m`" = "x64" ]; then
+  if [ "`uname -m`" = "x86_64" ]; then
     assertEquals "testBbrcClassification" "$h" "cf5a20167243312554c26127af22954e"
   else
     assertEquals "testBbrcClassification" "$h" "aad799b68060d8a6262230aae954f44c"
@@ -63,7 +63,8 @@ oneTimeSetUp()
 
 tearDown()
 {
-  rm -fr "${testdir}"
+  echo -n ""
+  #rm -fr "${testdir}"
 }
 
 # load and run shUnit2
