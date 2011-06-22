f7074308a6056f0813c9a2b0be429db2325b3cb9
Andreas Maunz <andreas@maunz.de>
2011-06-22 12:01:23 +0200

    Fixed default value for chisq

--- libbbrc/bbrc.cpp
+++ libbbrc/bbrc.cpp
@@ -171,7 +171,7 @@ void Bbrc::Defaults() {
     fm::bbrc_nr_hits = false;
 
     // BBRC
-    fm::bbrc_chisq->sig = 3.84146;
+    fm::bbrc_chisq->sig = -1.0;
     fm::bbrc_ks->sig = 0.95;
     fm::bbrc_do_backbone = true;
     fm::bbrc_adjust_ub = true;
