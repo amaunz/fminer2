0d18506a3d9880b5812cb468fcee9a371e8200e5
Andreas Maunz <andreas@maunz.de>
2011-08-31 14:19:00 +0200

    Fixed stopping reading on failing conversion

--- liblast/last.cpp
+++ liblast/last.cpp
@@ -323,6 +323,7 @@ bool Last::AddCompound(string smiles, unsigned int comp_id) {
   OBMol mol;
   if (!conv.Read(&mol)) {
     cerr << "Could not convert '" << smiles << "' (leaving out)." << endl;
+    return false;
   }
   conv.SetOptions("w",OBConversion::OUTOPTIONS);
   string inchi = conv.WriteString(&mol);
